3a4204dd45b46925032614f8b1ce58dab9d66641
ShahbazShaddy <shahbazghafil@gmail.com>
2024-11-09 22:33:09 +0500

    Refactor MongoDB connection to use environment variable for URI

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://shahbazghafil:fzVcpHDEadtOGWmj@cluster0.r5w2j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
